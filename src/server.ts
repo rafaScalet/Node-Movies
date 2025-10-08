ee2b037c07878929739c1093da9800325e48550e
Rafael Scalet <114953004+rafaScalet@users.noreply.github.com>
2025-10-08 18:19:40 -0300

    add and configure fastify

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,53 @@
-console.log("hello world");
+import { fastify } from "fastify";
+import { writeFile } from "node:fs/promises";
+import { resolve } from "node:path";
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+
+const dev = process.env.NODE_ENV !== "production";
+
+const app = fastify({
+  logger: dev ? { transport: { target: "pino-pretty" } } : true,
+});
+
+await app.register(fastifyCors, { origin: "*" });
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Node Movies",
+      description: "The Backend for Node Movies application",
+      version: "1.0.0",
+    },
+    tags: [],
+    servers: [
+      {
+        url: "http://localhost:3333",
+        description: "Development Server",
+      },
+    ],
+    externalDocs: {
+      url: "https://swagger.io",
+      description: "Find more info here",
+    },
+  },
+});
+
+await app.ready();
+
+if (dev) {
+  const filePath = resolve("openapi.json");
+  const spec = JSON.stringify(app.swagger(), null, 2);
+
+  writeFile(filePath, spec, "utf-8").then(() => {
+    app.log.info(`OpenAPI file saved on ${filePath}`);
+  });
+}
+
+app.listen({ port: 3333, host: "0.0.0.0" }, (err) => {
+  console.clear();
+
+  if (err) {
+    app.log.error(err);
+  }
+});
