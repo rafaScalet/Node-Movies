a10548e905b3f1c6e7f7830a127e63c8e1c334dd
Rafael Scalet <114953004+rafaScalet@users.noreply.github.com>
2025-10-25 08:48:11 -0300

    feat: add more info for users schemas

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const UserSchema = z.object({
-  id: z.int().default(1),
+  id: z.int(),
   name: z.string(),
   email: z.email(),
   password: z.string().min(8),
@@ -11,6 +11,20 @@ export const UserRequestSchema = UserSchema.omit({ id: true });
 
 export const UserResponseSchema = UserSchema.omit({ password: true });
 
-z.globalRegistry.add(UserSchema, { id: "user" });
-z.globalRegistry.add(UserRequestSchema, { id: "user-request" });
-z.globalRegistry.add(UserResponseSchema, { id: "user-response" });
+z.globalRegistry.add(UserSchema, {
+  id: "User",
+  title: "User Schema",
+  description: "User Related Schema",
+});
+
+z.globalRegistry.add(UserRequestSchema, {
+  id: "UserRequest",
+  title: "User Request Schema",
+  description: 'Schema to pass a User in the "body" field',
+});
+
+z.globalRegistry.add(UserResponseSchema, {
+  id: "UserResponse",
+  title: "User Response Schema",
+  description: "Schema to use in the response field",
+});
