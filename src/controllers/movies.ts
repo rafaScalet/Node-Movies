603976546979cd1bb59a7ab6091296baddb3d02a
MariaMariEdu <Maric.brito15@gmail.com>
2025-10-30 18:32:45 -0300

    feat: Route movies

--- src/controllers/movies.ts
+++ src/controllers/movies.ts
@@ -2,6 +2,7 @@ import { FastifyTypedInstance } from "@/config";
 import { MovieRequestSchema } from "@/schemas/movie";
 import { OkResponseSchema } from "@/schemas/response";
 import { create } from "@/services/movies/create";
+import { list } from "@/services/movies/list";
 
 export function MoviesController(app: FastifyTypedInstance) {
     app.post(
@@ -11,10 +12,19 @@ export function MoviesController(app: FastifyTypedInstance) {
                 body: MovieRequestSchema,
                 tags: ["movies"],
                 response: {
-                    201: OkResponseSchema.describe("Created").omit({value: true})
+                    201: OkResponseSchema.describe("Created").omit({ value: true })
                 },
             },
         },
         create
-    )
+    ),
+        app.get(
+            "/movies",
+            {
+                schema: {
+                    tags: ["movies"],
+                },
+            },
+            list
+        )
 }
