eba9a9441a200cd0fe0cc5b2df6158e6774ca90c
AngeloModanez <angelomodanez15@gmail.com>
2025-10-30 15:16:16 -0300

    feat: Created movie schema

---
+++ src/schemas/movie.ts
@@ -0,0 +1,42 @@
+import { z } from "zod";
+
+export const MovieSchema = z.object({
+  id: z.int(),
+  title: z.string().min(1),
+  description: z.string(),
+  genres: z.array(z.string()),
+  year: z.number().int().min(1895).max(9999),
+  duration: z.number().int().min(1).max(26000),
+  age: z.string(),
+  poster: z.url(),
+  link: z.url()
+})
+
+export const MovieRequestSchema = MovieSchema.omit({ id: true });
+
+export const MovieResponseSchema = MovieSchema;
+
+export type Movie = z.infer<typeof MovieSchema>;
+
+export type MovieRequest = z.infer<typeof MovieRequestSchema>;
+
+export type MovieResponse = z.infer<typeof MovieResponseSchema>;
+
+z.globalRegistry.add(MovieSchema, {
+  id: "Movie",
+  title: "Movie Schema",
+  description: "Movie Related Schema",
+});
+
+z.globalRegistry.add(MovieRequestSchema, {
+  id: "MovieRequest",
+  title: "Movie Request Schema",
+  description: 'Schema to pass a Movie in the "body" field',
+});
+
+
+z.globalRegistry.add(MovieResponseSchema, {
+  id: "MovieResponse",
+  title: "Movie Response Schema",
+  description: "Schema to use in the response field",
+});
