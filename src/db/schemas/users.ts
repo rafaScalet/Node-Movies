81524c4aaab151ab8dd797d09b4d67df9ac4ccbd
Rafael Scalet <114953004+rafaScalet@users.noreply.github.com>
2025-11-01 00:49:14 -0300

    feat: sign-up integration

--- src/db/schemas/users.ts
+++ src/db/schemas/users.ts
@@ -4,11 +4,11 @@ import { check, int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const users = sqliteTable(
   "users",
   {
-    id: int("id").primaryKey(),
+    id: int("id").primaryKey({ autoIncrement: true }),
     name: text("name").notNull(),
     email: text("email").notNull().unique(),
     password: text("password").notNull(),
-    role: text("roles", { enum: ["admin", "normal"] }).notNull(),
+    role: text("roles", { enum: ["admin", "user"] }).notNull(),
   },
   (table) => [
     check("roles_check_one", sql`${table.role} IN ('admin', 'normal')`),
--- src/services/auth/sign-up.ts
+++ src/services/auth/sign-up.ts
@@ -1,61 +1,35 @@
-import { User, UserRequest } from "@/schemas/user"
-import { FastifyReply, FastifyRequest } from "fastify"
-
-const users: Array<User> = [
-  {
-    id: 1,
-    name: "Murilo",
-    email: "murilo@gmail.com",
-    password: "murilo123",
-    role: "user",
-  },
-  {
-    id: 2,
-    name: "Seiya",
-    email: "seiya@gmail.com",
-    password: "seiya123",
-    role: "admin",
-  },
-]
+import { UserRequest } from "@/schemas/user";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { db } from "@/db/connections";
+import { schema } from "@/db/schemas";
 
 export async function singUp(
   req: FastifyRequest<{ Body: UserRequest }>,
-  res: FastifyReply
+  res: FastifyReply,
 ) {
-  const { name, email, password, role } = req.body
+  try {
+    const newUser = req.body;
 
-  const userAlreadyExits = users.find((data) => data.email === email)
+    const userAlreadyExits = await db.query.users.findFirst({
+      where: (data, { eq }) => eq(data.email, newUser.email),
+    });
 
-  if (  userAlreadyExits) {
-    return res.code(409).send({
-      error: "Conflict",
-      message: "User with this email already exists!",
-      statusCode: 409,
-    })
+    if (userAlreadyExits) {
+      return res.code(409).send({
+        error: "Conflict",
+        message: "User with this email already exists!",
+        statusCode: 409,
+      });
+    }
+
+    const createdUser = await db.insert(schema.users).values(newUser);
+
+    return res.code(201).send({
+      value: { createdUser },
+      message: "User created successfully",
+      statusCode: 201,
+    });
+  } catch (error) {
+    res.code(505).send(error);
   }
-
-  const lastUserId = users.length > 0 ? users[users.length - 1].id : 0
-
-  const newUser: User = {
-    id: lastUserId + 1,
-    name,
-    email,
-    password,
-    role,
-  }
-
-  users.push(newUser)
-
-  const UserResponse = {
-    id: newUser.id,
-    name: newUser.name,
-    email: newUser.email,
-    role: newUser.role,
-  }
-
-  return res.code(201).send({
-    value: { UserResponse },
-    message: "User created successfully",
-    statusCode: 201,
-  })
 }
