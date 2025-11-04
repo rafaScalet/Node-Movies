741ecde43e353bb2acbadda4f3803673298208c5
Angelo Ferraz Modanez <angelomodanez15@gmail.com>
2025-11-04 20:26:24 -0300

    style: format movies controller

--- src/controllers/movies.ts
+++ src/controllers/movies.ts
@@ -1,30 +1,33 @@
 import { FastifyTypedInstance } from "@/config";
 import { MovieRequestSchema } from "@/schemas/movie";
-import { OkResponseSchema } from "@/schemas/response";
+import { ErrorResponseSchema, OkResponseSchema } from "@/schemas/response";
 import { create } from "@/services/movies/create";
 import { list } from "@/services/movies/list";
 
 export function MoviesController(app: FastifyTypedInstance) {
-    app.post(
-        "/addMovie",
-        {
-            schema: {
-                body: MovieRequestSchema,
-                tags: ["movies"],
-                response: {
-                    201: OkResponseSchema.describe("Created").omit({ value: true })
-                },
-            },
+  app.post(
+    "/addMovie",
+    {
+      schema: {
+        body: MovieRequestSchema,
+        tags: ["movies"],
+        response: {
+          201: OkResponseSchema.describe("Created").omit({ value: true }),
+          409: ErrorResponseSchema.describe("Conflict")
         },
-        create
-    ),
-        app.get(
-            "/movies",
-            {
-                schema: {
-                    tags: ["movies"],
-                },
-            },
-            list
-        )
+      },
+    },
+    create,
+  );
+
+  app.get(
+    "/movies",
+    {
+      schema: {
+        tags: ["movies"],
+        200: OkResponseSchema.describe("Ok")
+      },
+    },
+    list,
+  );
 }
