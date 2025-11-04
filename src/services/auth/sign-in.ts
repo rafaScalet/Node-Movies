d8f7941a97089341c5531cc6590769a67540b649
Rafael Scalet <114953004+rafaScalet@users.noreply.github.com>
2025-11-04 15:10:27 -0300

    feat: Sign-up integration (#31)
    * feat: sign-up integration
    
    * feat: better return and schema
    
    add the token for user in response.value field and change the user
    schema to a record instead of an optional object
    
    * fix: use a better name for routes
    
    It is good practice to use only lowercase letters for routes, as they
    are case-sensitive, and this can generate several errors related to
    "human action"
    
    * feat: use uuid instead of int
    
    * feat: better seed for auth and drop DB script
    
    * feat: sign-in integration
    
    * feat: hash for a cryptographic passoword
    
    * feat: add the hash in the seed too
    
    all the mock users now have the same password, 12345678
    
    * fix: user role in DB schema

--- src/services/auth/sign-in.ts
+++ src/services/auth/sign-in.ts
@@ -1,36 +1,45 @@
-import { User, UserRequest } from "@/schemas/user"
-import { FastifyReply, FastifyRequest } from "fastify"
-
-const users: Array<User> = []
+import { db } from "@/db/connections";
+import { UserRequest } from "@/schemas/user";
+import { FastifyReply, FastifyRequest } from "fastify";
+import argon2 from "argon2";
 
 export async function signIn(
   req: FastifyRequest<{ Body: UserRequest }>,
-  res: FastifyReply
+  res: FastifyReply,
 ) {
-  const { name, email, password } = req.body
-  const user = users.find((data) => data.email === email)
+  try {
+    const { email, password, name, role } = req.body;
 
-  if (!user) {
-    return res.code(404).send({
-      error: "Not Found",
-      message: "User not found!",
-      statusCode: 404,
-    })
+    const user = await db.query.users.findFirst({
+      where: (data, { eq }) => eq(data.email, email),
+    });
+
+    if (!user) {
+      return res.code(404).send({
+        error: "Not Found",
+        message: "User not found!",
+        statusCode: 404,
+      });
+    }
+
+    const passwordIsCorrect = await argon2.verify(user.password, password);
+
+    if (!passwordIsCorrect) {
+      return res.code(401).send({
+        error: "Unauthorized",
+        message: "Invalid user password!",
+        statusCode: 401,
+      });
+    }
+
+    const token = req.server.jwt.sign({ name, email, role });
+
+    return res.code(200).send({
+      value: { token },
+      message: "Authorized",
+      statusCode: 200,
+    });
+  } catch (error) {
+    res.code(505).send(error);
   }
-
-  if (user.password !== password) {
-    return res.code(401).send({
-      error: "Unauthorized",
-      message: "Invalid user password!",
-      statusCode: 401,
-    })
-  }
-
-  const token = req.server.jwt.sign({ name, email })
-
-  return res.code(200).send({
-    value: { token },
-    message: "Authorized",
-    statusCode: 200,
-  })
 }
